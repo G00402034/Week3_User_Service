b68645a4ed15c411f5a94dd51d48445a28c0dddd
G00402034 <G00402034@atu.ie>
2023-11-28 18:46:42 +0000

    Added class book

---
+++ src/main/java/ie/atu/week_3_requestbody/Week3RequestbodyApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week_3_requestbody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3RequestbodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week3RequestbodyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week_3_requestbody/Week3RequestbodyApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week_3_requestbody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3RequestbodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
